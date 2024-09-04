67258f242b8b51a49dd0aa96a99dacc826d9a4f2
dawn <dawn@berkeley.edu>
2024-09-04 15:47:20 +0000

    Updated src/data/resources/resources.js (added 22 lines, deleted 7 lines).

--- src/data/resources/resources.js
+++ src/data/resources/resources.js
@@ -2,11 +2,26 @@ exports.data = [
   {
     title : "The Urban Displacement Project",
     link : "https://www.urbandisplacement.org/",
-    description: "",
+    description: "The Urban Displacement Project conducts community-driven research to understand the nature of gentrification, culminating in research and mapping tools meant to inform advocacy and policy. Our tool's 'Displacement Risk' variable is built on the Urban Displacement Project's displacement typology maps."
   },
   {
-    title : "",
-    link : "",
-    description: ""
-  } 
+    title : "Bay Area Resilience Hotspots",
+    link : "https://www.greenbelt.org/hotspots/#hotspots",
+    description: "This project by the Greenbelt Alliance identifies Bay Area communities that are particularly at risk of climate change hazards, outlining what can be done in each community to best mitigate them. Our tool draws from these hotspots in the 'Hotspots' tab."
+  } ,
+  {
+    title: "Bay Area Greenprint: Analyze & Report",
+    link: "https://www.bayareagreenprint.org/report/",
+    description: "This tool by Bay Area Greenprint is a screening-level evaluation tool that allows on-demand analysis of various environmental and socioeconomic indicators in any user-defined area."
+  },
+  {
+    title: "Vital Signs",
+    link: "https://vitalsigns.mtc.ca.gov/categories/land-people",
+    description: "Vital Signs is an interactive website by the Metropolitan Transportation Commission, detailing beautifully laid out data on population, transportation modes, economy, the environment, giving an overview of the Bay Area's socioeconomic and environmental landscapes."
+  },
+  {
+    title: "The Nature Conservancy: Resilient Land Mapping",
+    link : "https://www.maps.tnc.org/resilientland/#/explore",
+    description: "This mapping tool classifies the entire United States' resilience to climate change, including developed land, undeveloped land, and the status of vital tidal habitats."
+  }
 ]
